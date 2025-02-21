921ce9dc3c8f6fa3b291359f87c0cf72bdfc4bc6
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-02-20 21:15:55 -0800

    extract message ID of a patch

--- analyze_commitfest.py
+++ analyze_commitfest.py
@@ -1,5 +1,6 @@
 
-from scrape import extract_commitfest_patch_ids
+from scrape import extract_commitfest_patch_ids, get_patch_info
+from pprint import pprint
 
 def analyze_commitfest(id):  
     #1. Scrape the list of patches.
@@ -7,6 +8,12 @@ def analyze_commitfest(id):
     patch_ids = extract_commitfest_patch_ids(url)
 
     print(patch_ids)
+
+    patch_info = get_patch_info(patch_ids[0])
+
+    pprint(patch_info)
+
+
 
 if __name__ == '__main__':
     analyze_commitfest(52)
--- scrape.py
+++ scrape.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+# Scraping the mailing list 
+
 def scrape_text_from_div(url, id):
     response = requests.get(url)
     
@@ -19,7 +21,7 @@ def scrape_text_from_div(url, id):
     else:
         raise ValueError(f"No div with id '{id}' found.")
 
-
+# Scraping the commitfest page
 def extract_commitfest_patch_ids(url):
     response = requests.get(url)
     response.raise_for_status()  # Raise an error for bad responses
@@ -36,4 +38,36 @@ def extract_commitfest_patch_ids(url):
             ids.append(int(match.group(1)))  # Extract and convert to int
 
     return ids
+
+# Scraping the Patch page
+def get_patch_info(patch_id):
+    url = f'https://commitfest.postgresql.org/patch/{patch_id}/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    message_ids = _helper_get_patch_message_ids(soup)
+
+
+    return {
+        "patch_id": patch_id,
+        "message_ids": message_ids,
+    }
+
+def _helper_get_patch_message_ids(soup):
+    # Find the table row where <th> contains "Emails"
+
+    message_ids = set()
+
+    # Regex pattern to match the desired links
+    pattern = re.compile(r'https://www.postgresql.org/message-id/flat/(.*)')
+
+    # Find all links that match the pattern
+    ids = []
+    for a_tag in soup.find_all('a', href=True):
+        match = pattern.match(a_tag['href'])
+        if match:
+            ids.append(match.group(1)) 
+
+    return list(set(ids))
+
 
