b225e78e05a5f4692b5b05a4d9f4507c99df21f1
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-02-21 16:40:25 -0800

    ingesting the commitstats data to postgres for analysis

---
+++ repository/ingest_table_stats.sql
@@ -0,0 +1,18 @@
+-- 1. Drop table if it already exists
+DROP TABLE IF EXISTS commitstats;
+
+-- 2. Create the new table
+CREATE TABLE commitstats (
+    author TEXT,
+    file TEXT,
+    additions INT,
+    deletions INT,
+    commit TEXT,
+    date TIMESTAMPTZ
+);
+
+-- 3. (Optional) Truncate if we want to be sure we're starting from an empty table
+TRUNCATE TABLE commitstats;
+
+-- 4. Ingest data from the JSON file using jq to transform JSON -> CSV
+\copy commitstats (author, file, additions, deletions, commit, date) FROM PROGRAM 'jq -r ".[] | @csv" repo_history.json' CSV QUOTE '"' ESCAPE '\';
