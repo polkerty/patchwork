9cae36162a81c419fdb8ed77405a57733cc2dadc
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-02-23 10:25:28 -0800

    calculate IDF

--- repository/tf-idf.sql
+++ repository/tf-idf.sql
@@ -1,9 +1,28 @@
+begin;
+
 drop materialized view if exists contrib_tf;
 
 create  materialized view contrib_tf as 
     select 
-        normalize_name_email(author) contrib_name,
+        normalize_name_email(author) author,
          file, 
          sum(log(1 + additions + deletions)) tf 
     from commitstats 
     group by 1, 2;
+
+
+drop materialized view if exists contrib_idf;
+
+create materialized view contrib_idf as 
+
+    with file_cnt as (
+        select file, count(*) cnt
+        from contrib_tf
+        group by 1
+    )
+    select 
+        file,
+        (select count(distinct author) from contrib_tf) / cnt idf
+    from file_cnt;
+
+commit;
