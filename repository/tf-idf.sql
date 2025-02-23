3e0f65462eb898d9ef7fc819e7349affb3113650
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-02-23 11:21:59 -0800

    some improvements to tf-idf views and calculations

--- repository/tf-idf.sql
+++ repository/tf-idf.sql
@@ -7,7 +7,7 @@ create  materialized view contrib_tf as
     select 
         normalize_name_email(author) author,
          file, 
-         sum(log(1 + additions + deletions)) tf 
+         sum(ln(1 + additions + deletions)) tf 
     from commitstats 
     group by 1, 2;
 
@@ -30,20 +30,28 @@ create materialized view contrib_idf as
 drop materialized view if exists per_file_tf_idf cascade;
 
 create materialized view perfile_tf_idf as
-    select a.message_id message_id, tf.file file, tf.author reviewer, tf.tf * idf.idf tf_idf
+    select 
+        a.message_id message_id, 
+        p.patch patch,
+        tf.file file, 
+        tf.author reviewer, 
+        a.additions,
+        a.deletion,
+        tf.tf * idf.idf unweighted_tf_idf,
+        ln(1 + a.additions + a.deletion) * tf.tf * idf.idf tf_idf
     from 
         contrib_tf tf
         left join contrib_idf idf on tf.file = idf.file
         left join attachment_stats a on a.file = tf.file
+        left join patch_message p on p.message = a.message_id
 ;
 
 drop materialized view if exists contrib_tf_idf cascade;
 
 create materialized view contrib_tf_idf as 
-    select reviewer, p.patch, sum(tf_idf)
+    select reviewer, patch, sum(tf_idf)
     from
         perfile_tf_idf f
-        left join patch_message p on p.message = f.message_id
     group by 1, 2
     order by 1 asc, 2 desc;
 ;
