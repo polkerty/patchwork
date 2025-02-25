db81a713c59d68bb1e3cd1592836279169caaa39
Jacob Brazeal <jacob.brazeal@gmail.com>
2025-02-25 13:54:52 -0800

    Clean up logs a bit

--- commitfest/clusters.py
+++ commitfest/clusters.py
@@ -3,12 +3,11 @@ from analyze_thread import parse_thread
 from cache import cache_results
 from worker import run_jobs
 
+from pprint import pprint
+
 cache_results()
 def get_valid_repo_threads(repo, commits):
     commit_and_thread = get_threads_of_last_n_commits(repo, commits)
-
-    print(f'Got {len(commit_and_thread)} threads')
-    print(commit_and_thread[0]) # sample
 
     # 2. fetch threads
     message_ids = [t["thread"] for t in commit_and_thread]
@@ -17,15 +16,16 @@ def get_valid_repo_threads(repo, commits):
 
     # simply discard threads that failed because of a 404 or other reason
     valid_threads = {thread: results for thread, results in threads.items() if 'error' not in results}
-    print(f"Success rate in threads: {len(valid_threads)}/{len(threads)} ({round(100 * len(valid_threads)/len(threads))}%.)")
+    print(f"Success rate in threads: {len(valid_threads)}/{len(threads)} ({round(100 * len(valid_threads)/len(threads))}%).")
 
     return valid_threads
 
 def main():
     threads = get_valid_repo_threads('~/postgres/postgres', 10000)
 
-    for thread in threads:
-        print(thread)
+    print(f'Got {len(threads)} threads')
+    pprint(list(threads.items())[0]) # sample
+
 
 if __name__ == "__main__":
     main()
